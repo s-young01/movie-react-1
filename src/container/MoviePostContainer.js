af61f82ab1db7df59303d22ec42b01574c00b81d
namminimi <tpdud0949@gmail.com>
2023-02-07 17:51:07 +0900

    kwon movie
    moviepost moviepost container movie detail newmoviepage 추가 수정

--- src/container/MoviePostContainer.js
+++ src/container/MoviePostContainer.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDatas } from '../moduls/moviePost';
+import NewMoviePage from '../pages/NewMoviePage';
 
 const MoviePostContainer = () => {
-    const {loading, data, error} = useSelector(state=>state.moviePost.moviePost)
-    console.log(data)
+    const {loading, data, error} = useSelector(state => state.moviePost.moviePosts);
+    const dispatch = useDispatch();
+    console.log(data);
+
+    useEffect(() => {
+        dispatch(getDatas());
+    }, [dispatch]);
+    if(loading) return <div>로딩중입니다..</div>
+    if(error) return <div>에러가 발생했습니다.</div>
+    if(!data) return <div>데이터가 없습니다.</div>
     return (
-        <div>
-            
-        </div>
+        <NewMoviePage data={data}/>
     );
 };
 
---
+++ src/container/MoviePostDetail.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getData } from '../moduls/moviePost';
+import MovieDetail from '../pages/subpages/MovieDetail';
+
+const MoviePostDetail = () => {
+    const {no} = useParams();
+    console.log(`여기에요${no}`);
+    const {loading, data, error} = useSelector(state => state.moviePost.moviePost);
+    const dispatch = useDispatch();
+    console.log(data);
+    useEffect(() => {
+        dispatch(getData(no));
+    }, [dispatch,no]);
+    if(loading) return <div>로딩중입니다..</div>
+    if(error) return <div>에러가 발생했습니다.</div>
+    if(!data) return <div>데이터가 없습니다.</div>
+    return (
+        <MovieDetail data={data}/>
+    );
+};
+
+export default MoviePostDetail;
