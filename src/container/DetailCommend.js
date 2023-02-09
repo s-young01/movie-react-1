996b1679f4fd9dfef7464801d07ec937d1ad9ce6
s-young01 <119985133+s-young01@users.noreply.github.com>
2023-02-10 03:27:32 +0900

    kwon movie
    상세 페이지 리뷰

--- src/container/DetailCommend.js
+++ src/container/DetailCommend.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './DetailCommend.scss';
 import { Pagination } from 'antd';
+import { BiLike, BiDislike,BiCommentDetail, BiCommentEdit } from "react-icons/bi";
 
 const DetailCommend = () => {
     // input / textarea 상태관리
@@ -15,21 +16,37 @@ const DetailCommend = () => {
     return (
         <div className='commendbox'>
             <div className='titlezone'>
-                <h3>영화 리뷰</h3>
+                <h3>영화 리뷰<BiCommentDetail className='icon2'/></h3>
                 <nav>
-                    <span>추천 : 10</span>
-                    <span>비추천 : 3</span>
+                    <BiLike className='icon'/> : <span>10</span>
+                    <BiDislike className='icon'/> : <span>3</span>
                 </nav>
             </div>
             <div className='commendzone'>
                 <table className='commendtable'>
                     <thead>
                         <tr>
-                            <th>닉네임</th>
-                            <th>제목</th>
+                            <th width='30%'>작성자</th>
+                            <th width='70%'>제목</th>
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>권세영</td>
+                            <td>이 영화 너무 재밌네요 ~ ^^</td>
+                        </tr>
+                        <tr>
+                            <td>권세영</td>
+                            <td>이 영화 너무 재밌네요 ~ ^^</td>
+                        </tr>
+                        <tr>
+                            <td>권세영</td>
+                            <td>이 영화 너무 재밌네요 ~ ^^</td>
+                        </tr>
+                        <tr>
+                            <td>권세영</td>
+                            <td>이 영화 너무 재밌네요 ~ ^^</td>
+                        </tr>
                         <tr>
                             <td>권세영</td>
                             <td>이 영화 너무 재밌네요 ~ ^^</td>
@@ -39,7 +56,7 @@ const DetailCommend = () => {
                 <Pagination defaultCurrent={1} total={50} />
             </div>
             <div className='writezone'>
-                <h3>리뷰 쓰기</h3>
+                <h3>리뷰 쓰기<BiCommentEdit className='icon2'/></h3>
                 <div className='writebox'>
                     <span>제목</span>
                     <input maxLength={50} onChange={oninputHandeler}/>
@@ -51,8 +68,9 @@ const DetailCommend = () => {
                     <p>
                         <span>{textCount}</span>/ 500자
                     </p>
+                    <button>등록</button>
                 </div>
-                <button>등록</button>
+                
             </div>
         </div>
     );
