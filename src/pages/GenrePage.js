4742667af8997642b2253999953c25717854ff90
namminimi <tbvhdaos@naver.com>
2023-02-10 17:39:41 +0900

    nam movie
    장르별 영화목록 선택기능 추가

--- src/pages/GenrePage.js
+++ src/pages/GenrePage.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { API_URL } from '../config/apiurl';
 import "./NewMoviePage.scss";
+import "./GenrePage.scss"
 
 // div map 돌려서 뿌려줄 컴포넌트 만들기
 function NewMovieList({list}) {
@@ -41,39 +42,48 @@ function NewMovieList({list}) {
     );
 }
 
+
 const GenrePage = ({data}) => {
-    console.log(data)
+    //console.log(data)
+    const [isGenre, setGenre] = useState({
+        genrelist: [
+        {id: 1, genrelist:"전체", isDone: false},
+        {id: 2, genrelist:"액션", isDone: false},
+        {id: 3, genrelist:"호러/공포", isDone: false},
+        {id: 4, genrelist:"애니메이션", isDone: false},
+        {id: 5, genrelist:"스릴러/미스테리", isDone: false},
+        {id: 6, genrelist:"어드벤쳐", isDone: false},
+        {id: 7, genrelist:"판타지/다크판타지/슈퍼히어로/SF", isDone: false},
+        {id: 8, genrelist:"뮤지컬/음악", isDone: false},
+        {id: 9, genrelist:"시대극/전기", isDone: false},
+        {id: 10, genrelist:"멜로/로멘스/드라마/가족/스포츠", isDone: false},
+        {id: 11, genrelist:"코미디/블랙 코미디", isDone: false},
+        {id: 12, genrelist:"범죄/느와르", isDone: false},
+        {id: 13, genrelist:"전쟁/밀리터리", isDone: false},
+        {id: 14, genrelist:"재난/포스트 아포칼립스", isDone: false}
+    ]
+})
+
+    const onToggle = (id) => {
+        const newlist = isGenre.genrelist.map(gen=>gen.id == id ? {
+            ...gen,
+            isDone: !gen.isDone
+        }: gen.id != id && gen.isDone == true ? {
+            ...gen,
+            isDone: !gen.isDone 
+        }: gen)
+        console.log(newlist)
+        setGenre({
+            ...isGenre,
+            genrelist : newlist
+        });
+    }
     return (
         <div className="NewMovie_page">
             <h2>장르영화</h2>
-            <ul>
-                <li>액션</li>
-                <li>호러</li>
-                <li>애니메이션</li>
-                <li>스포츠</li>
-                <li>드라마</li>
-                <li>스릴러</li>
-                <li>어드벤쳐</li>
-                <li>SF</li>
-                <li>다큐멘터리</li>
-                <li>환타지</li>
-                <li>느와르</li>
-                <li>뮤지컬</li>
-                <li>시대극</li>
-                <li>멜로</li>
-                <li>로멘스</li>
-                <li>슈퍼히어로</li>
-                <li>밀리터리</li>
-                <li>전쟁</li>
-                <li>가족</li>
-                <li>블랙 코미디</li>
-                <li>재난</li>
-                <li>전기</li>
-                <li>포스트 아포칼립스</li>
-                <li>다크판타지</li>
-                <li>음악</li>
-                <li>공포</li>
-                <li>미스테리</li>
+            <ul className='genreUl'>
+                {isGenre.genrelist.map(gen=><li key={gen.id} value style={{fontWeight: gen.isDone ? "bold" :"normal" }}>
+                    <span onClick={()=>{onToggle(gen.id)}}>{gen.genrelist}</span></li>)}
             </ul>
             <div className="NewMovie_box">
                 {data.map(d => <NewMovieList key={d.mov_no} list={d}/>)}
