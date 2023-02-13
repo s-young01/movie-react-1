716ffcf140cfdcc452a01949a121bd4cb972c9b4
fedfs900 <ckdals900@gmail.com>
2023-02-13 18:26:17 +0900

    chang
    리부

--- src/pages/ReviewList.js
+++ src/pages/ReviewList.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { API_URL } from '../config/apiurl';
+import './ReviewList.scss';
 
 const ReviewList = ({list}) => {
     return (
-        <div>
-            <img src="images/poster2_1.jpg" alt=''/>
-            <h4>{list.title}<span>평점</span></h4>
-            <p>{list.desc}</p>
+        <div className='reviewlist'>
+                <div className='reviewbox'>
+                    <div className='imgbox'>
+                        <img src={`${API_URL}/${list.r_img}`} alt=''/>
+                    </div>
+                    <div className='reviewtext'>
+                        <div className='rev_title'>
+                            <nav className='titlesp'>
+                                <span>{list.r_title}</span>
+                            </nav>
+                            <nav className='rev_writen'>
+                                <span>작성자</span>
+                                <span>:</span>
+                                <span>{list.r_nickname}</span>
+                            </nav>
+                        </div>
+                        <div className='rev_desc'>
+                            <p>{list.r_desc}</p>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 };
